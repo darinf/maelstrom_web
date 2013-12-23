a89d31a85546124f3e2bdcf9d6d790eb17fbd630
Darin Fisher <darin@chromium.org>
2013-12-22 22:17:16 -0800

    Prune

--- SDL/SDL_init.cpp
+++
@@ -1,4 +0,0 @@
-#include "SDL_init.h"
-
-int SDL_Init(int init_flags) { return 0; }
-void SDL_Quit() {}
--- port.c
+++
@@ -1,5 +0,0 @@
-#include <signal.h>
-
-sighandler_t signal(int signum, sighandler_t handler) {
-  return 0;
-}
